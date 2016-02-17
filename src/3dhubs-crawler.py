8464e74f49a2d962625c241f0910c7b425c8c36a
Xilin Sun <s.sn.giraffe@gmail.com>
2016-02-16 21:52:53 -0800

    Remove unused files

--- src/3dhubs-crawler.py
+++ src/3dhubs-crawler.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # 3dhubs-crawler.py
 # by Xilin Sun
-# 
+# April 2015
+
+
 import urllib2
 import bs4
 import requests
@@ -9,16 +11,18 @@ import pandas as pd
 import json
 from StringIO import StringIO
 
+# Get list of 3d hubs
 def find_hub_lists(URL):
     hub_lists = []
     response = requests.get(URL)
     soup = bs4.BeautifulSoup(response.text)
-    cell = soup.findAll('span', {'class':"station-hub-title"})
+    cell = soup.findAll('span', {'class': "station-hub-title"})
     for i in cell:
         hub_name = i.text[:-6]
         hub_lists.append(hub_name)
 
     return hub_lists
+
 
 def read_hub_info(h, soup):
     # First, we want to get the information from the sidebar.
@@ -27,24 +31,27 @@ def read_hub_info(h, soup):
     hub_info = []
     hub_info.append(str(h))
 
-    sidebar = soup.findAll(('aside', {'id':'sidebar'}))
+    sidebar = soup.find_all(('aside', {'id': 'sidebar'}))
 
     try:
-        specialties = filter(None, sidebar[0].findAll('div', {'id':'block-hubs3d-hub-hub-specialties' })[0].findAll('div', {'class': 'field-items'})[0].text.split('\n'))
+        specialties = filter(None, sidebar[0].findAll('div', {'id': 'block-hubs3d-hub-hub-specialties'})[
+                             0].findAll('div', {'class': 'field-items'})[0].text.split('\n'))
         specialties = [x.encode('UTF8') for x in specialties]
         hub_info.append(specialties)
     except:
         hub_info.append([])
 
     try:
-        about = filter(None, sidebar[0].findAll('div', {'id':'block-hubs3d-hub-hub-about' })[0].findAll('div', {'class': 'field-items'})[0].text.split('\n'))
+        about = filter(None, sidebar[0].findAll('div', {'id': 'block-hubs3d-hub-hub-about'})[
+                       0].findAll('div', {'class': 'field-items'})[0].text.split('\n'))
         about = [x.encode('UTF8') for x in about]
         hub_info.append(about)
     except:
         hub_info.append([])
 
     try:
-        properties = filter(None, sidebar[0].findAll('div', {'class':'hub-properties' })[0].findAll('div', {'class': 'field-item'}))
+        properties = filter(None, sidebar[0].findAll(
+            'div', {'class': 'hub-properties'})[0].findAll('div', {'class': 'field-item'}))
         property_list = []
         for p in properties:
             property_list.append(str(p.text))
@@ -52,14 +59,15 @@ def read_hub_info(h, soup):
     except:
         hub_info.append([])
 
-    # The location is used in one of the scripts, so we first find all scripts and then filter them by contents.
+    # The location is used in one of the scripts, so we first find all scripts
+    # and then filter them by contents.
     try:
         for s in soup.findAll('script'):
             if 'Drupal.settings' in str(s.contents):
                 text = s.text[31:-2]
                 io = StringIO(text)
                 text = json.load(io)
-                #print text
+                # print text
                 for loc in text['getlocations']['key_1']['latlons']:
                     if 'default' in filter(None, loc):
                         hub_info.append(loc[:2])
@@ -68,13 +76,16 @@ def read_hub_info(h, soup):
 
     # This part is to extract number of reviews and reviews content.
     try:
-        review = soup.findAll('div', {'id': 'block-hubs3d-review-hubs3d-hub-reviews-full'})
-        review_count = review[0].findAll('span', {'class': 'votecount'})[0].text.split()[2]
+        review = soup.findAll(
+            'div', {'id': 'block-hubs3d-review-hubs3d-hub-reviews-full'})
+        review_count = review[0].findAll('span', {'class': 'votecount'})[
+            0].text.split()[2]
         hub_info.append(review_count.encode('UTF8'))
 
         review_list = []
         for r in review[0].findAll('div', {'class': 'review-content'}):
-            review_list.append(r.text.replace('\n', '').lstrip().encode('UTF8'))
+            review_list.append(r.text.replace(
+                '\n', '').lstrip().encode('UTF8'))
         hub_info.append(review_list)
     except:
         hub_info.append(0)
@@ -82,10 +93,13 @@ def read_hub_info(h, soup):
 
     return hub_info
 
+
 def read_printer_info(h, soup):
 
     # Get information on printers and store it
-    # It contains information such as printerName, offlineStatus, deliveryTime, startupCost and material properties (materialName, materialCost and materialColor)
+    # It contains information such as printerName, offlineStatus,
+    # deliveryTime, startupCost and material properties (materialName,
+    # materialCost and materialColor)
 
     printers_info = []
 
@@ -97,7 +111,8 @@ def read_printer_info(h, soup):
             printers_offline.append('offline')
 
     printerDetails = soup.find_all("div", class_="group-printer-details")
-    materialDetails= soup.find_all("div", class_="field-name-field-material-collection")
+    materialDetails = soup.find_all(
+        "div", class_="field-name-field-material-collection")
     numberPrinters = len(printerDetails)
 
     for n in range(numberPrinters):
@@ -107,9 +122,12 @@ def read_printer_info(h, soup):
         printerName = printerDetails[n].findAll('h2')[0].text.split('\n')
         printer_info.append(printerName)
 
-        deliveryTime = printerDetails[n].find_all("div", class_="field-item item-1 even")[0].text
-        startupCost = printerDetails[n].find_all("div", class_="field-item item-1 even")[1].text
-        printResolution = printerDetails[n].find_all("div", class_="field-item item-1 even")[2].text
+        deliveryTime = printerDetails[n].find_all(
+            "div", class_="field-item item-1 even")[0].text
+        startupCost = printerDetails[n].find_all(
+            "div", class_="field-item item-1 even")[1].text
+        printResolution = printerDetails[n].find_all(
+            "div", class_="field-item item-1 even")[2].text
 
         printer_info.extend([deliveryTime, startupCost, printResolution])
 
@@ -123,6 +141,7 @@ def read_printer_info(h, soup):
 
     return printers_info
 
+
 def main():
 
     # Focusing on NYC at the moment
@@ -130,13 +149,14 @@ def main():
     response = requests.get(URL)
     soup = bs4.BeautifulSoup(response.text)
 
-	# Find how many pages are there for the hub lists.
+    # Find how many pages are there for the hub lists.
     pages = soup.findAll('li', {'class': 'pager-current'})
     num_page = int(pages[0].text.split('of')[-1])
 
     # The structure of the URL:
     # For the first page is 'https://www.3dhubs.com/3dprint?hf[data][location][search]=New%20York%20City&hf[data][location][lat]=40.7143&hf[data][location][lon]=-74.006&hf[data][location][radius]=50'
-    # The latter page has the format like 'https://www.3dhubs.com/3dprint?page={}&hf[data][location][search]=New%20York%20City&hf[data][location][lat]=40.7143&hf[data][location][lon]=-74.006&hf[data][location][radius]=250'.format(num_page)
+    # The latter page has the format like
+    # 'https://www.3dhubs.com/3dprint?page={}&hf[data][location][search]=New%20York%20City&hf[data][location][lat]=40.7143&hf[data][location][lon]=-74.006&hf[data][location][radius]=250'.format(num_page)
 
     # Given a URL, crawl all the hubs' names.
     hub_lists = []
@@ -144,7 +164,8 @@ def main():
         if i == 0:
             URL = 'https://www.3dhubs.com/3dprint?hf[data][location][search]=New%20York%20City&hf[data][location][lat]=40.7143&hf[data][location][lon]=-74.006&hf[data][location][radius]=50'
         else:
-            URL = 'https://www.3dhubs.com/3dprint?page={}&hf[data][location][search]=New%20York%20City&hf[data][location][lat]=40.7143&hf[data][location][lon]=-74.006&hf[data][location][radius]=50'.format(i)
+            URL = 'https://www.3dhubs.com/3dprint?page={}&hf[data][location][search]=New%20York%20City&hf[data][location][lat]=40.7143&hf[data][location][lon]=-74.006&hf[data][location][radius]=50'.format(
+                i)
         hub_lists.extend(find_hub_lists(URL))
 
     ################### Now we have all unique hub names.#####################
@@ -153,10 +174,12 @@ def main():
 
     # The URL for each hub has the format as 'https://www.3dhubs.com/new-york/hubs/{}'.format(hub_name)
     # For example, hub name 'Olivero Design' has the URL 'https://www.3dhubs.com/new-york/hubs/olivero-design'
-    # Thus, we here clean each hub name so that it can be directly used to track the hub's website.
+    # Thus, we here clean each hub name so that it can be directly used to
+    # track the hub's website.
     clean_hub_lists = []
     for i in hub_lists:
-        clean_hub_lists.append('-'.join(i.replace('+', '').replace('_', '').replace("'", '').replace('.', '').replace('The', '').strip().split()))
+        clean_hub_lists.append('-'.join(i.replace('+', '').replace('_', '').replace(
+            "'", '').replace('.', '').replace('The', '').strip().split()))
 
     all_hub_info = pd.DataFrame()
     all_hub_printer_info = pd.DataFrame()
@@ -167,17 +190,21 @@ def main():
         soup = bs4.BeautifulSoup(response.text)
 
         print 'Save information for hub {}'.format(h)
-        all_hub_info = pd.concat([all_hub_info, pd.DataFrame(read_hub_info(h, soup)).T])
+        all_hub_info = pd.concat(
+            [all_hub_info, pd.DataFrame(read_hub_info(h, soup)).T])
 
         print 'Save information for all printers in hub {}'.format(h)
-        all_hub_printer_info = pd.concat( [all_hub_printer_info, pd.DataFrame(read_printer_info(h, soup))])
+        all_hub_printer_info = pd.concat(
+            [all_hub_printer_info, pd.DataFrame(read_printer_info(h, soup))])
 
     print '******Save all hub information to file all_hub_info.csv*********'
-    all_hub_info.columns = ['hub_name', 'specialties', 'about', 'properties', 'location', 'num_reviews', 'reviews content']
+    all_hub_info.columns = ['hub_name', 'specialties', 'about',
+                            'properties', 'location', 'num_reviews', 'reviews content']
     all_hub_info.to_csv('all_hub_info.csv')
 
     print '******Save all hub information to file all_hub_printer_info.csv*********'
-    all_hub_printer_info.columns = ['hub_name', 'offline', 'printer_name', 'deliveryTime', 'startupCost', 'printResolution', 'material']
+    all_hub_printer_info.columns = ['hub_name', 'offline', 'printer_name',
+                                    'deliveryTime', 'startupCost', 'printResolution', 'material']
     all_hub_printer_info.to_csv('all_hub_printer_info.csv')
 
 if __name__ == '__main__':
